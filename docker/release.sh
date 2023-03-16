e1c7e76669e985208d034ad1ca9ee18313f27f47
Geapefurit <670884108@qq.com>
2023-03-16 17:28:05 +0800

    change dockerfile

--- docker/release.sh
+++ docker/release.sh
@@ -13,12 +13,12 @@ if [ "x" == "x$version" ]; then
 fi
 
 if [ "x" == "x$registry" ]; then
-  registry="uhub.service.ucloud.cn/entropypool"
+  registry="uhub.service.ucloud.cn"
 fi
 
 user=`whoami`
 if [ "$user" == "root" ]; then
-    docker push $registry/$service_name:$version
+    docker push $registry/entropypool/$service_name:$version
 else
-    sudo push $registry/$service_name:$version
+    sudo push $registry/entropypool/$service_name:$version
 fi
